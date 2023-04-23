0b85c6ef44871dc1946bf0ffc9077b7095377481
Justin T <98497366+DrGonzo79@users.noreply.github.com>
2023-04-22 23:54:39 -0500

    final changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,22 @@ import { logo } from '../assets';
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
-        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
-
-        <button type='button' onClick={() => window.open('https://github.com/drgonzo79')}>
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-20 object-contain" />
+        
+        <button type='button' onClick={() => window.open('https://github.com/drgonzo79/ai_summarizer')}
+        className='black_btn'>
           GitHub
         </button>
       </nav>
 
+      <h1 className="head_text">
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient'>DGC Digital & GPT-4</span>
+      </h1>
+      <h2 className="desc">
+      Discover the power of DGC Digital's innovative open-source article summarizer, designed to transform extensive articles into clear, concise summaries. Elevate your reading experience by quickly grasping essential information with ease and efficiency.
+      </h2>
     </header>
   )
 }
